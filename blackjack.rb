c48404030190f50fadcd6d9f3cea8851215287d2
Nikki Thean <nikki.thean@gmail.com>
2013-10-28 20:08:40 -0400

    Break down into smaller methods

--- blackjack.rb
+++ blackjack.rb
@@ -6,64 +6,96 @@
 
 
 class Blackjack
-
-	class Deck
-		attr_accessor :cards
-
-		SUITS = %w{ Clubs Diamonds Hearts Spades }
-		RANKS = %w{ 2 3 4 5 6 7 8 9 10 Jack Queen King Ace }
-
-		def initialize(n=1)
-			@cards = []
-			SUITS.cycle(n) do |s|
-				RANKS.each do |r|
-					@cards << "#{r} of #{s}"
-				end
-			end
-		end
-	end
 	
+	attr_accessor :deck, :no_of_decks, :card, :answer, :counter, :turns, :game_end
+
+	def initialize
+		@no_of_decks = 0
+		@counter = 0
+		@turns = 0
+		@game_end = false
+		
+		# welcome
+		# pick_a_card
+	end
+
+	def run
+		welcome
+		start_game
+		no_of_decks
+		pick_a_card
+	end
+
 	def welcome
 		puts "\nWelcome to my casino! \nWould you like to play a game of Blackjack?"
-		answer = gets.chomp.downcase
+	end
+
+	def get_input_string
+		@answer = gets.chomp.downcase
+	end
+
+	def get_input_integer
+		@answer = gets.chomp.to_i
+	end
+
+	def enter_yes_or_no
 		while ["yes", "y", "no", "n"].include?(answer) == false
 			puts "Please enter yes or no."
-			answer = gets.chomp.downcase
+			get_input_string
 		end
+	end
 
+	def start_game
+		get_input_string
+		enter_yes_or_no
 		case answer
 			when "yes", "y"
 				puts "\nHave a seat!\n\nDrawing up a chair..."
 				sleep(1)
-				puts "How many decks would you like to use?"
-				answer = gets.chomp.to_i - 0
-				while answer <= 0
-					puts "Please enter a positive integer."
-					answer = gets.chomp.to_i - 0
-				end
-				@no_of_decks = answer
 			when "no", "n"
 				puts "\nTerrible choice."
 				Kernel.exit
 		end
 	end
 
+	def enter_positive_integer
+		while answer <= 0
+			puts "Please enter a positive integer."
+			get_input_integer
+		end
+	end
+
+	def no_of_decks
+		puts "How many decks would you like to use?"
+		get_input_integer
+		enter_positive_integer
+		no_of_decks = answer
+		@deck = Deck.new(no_of_decks)
+	end
+
 	def deal
-		deck = Deck.new(@no_of_decks) #DO NOT instantiate here
-		card = deck.cards.sample
-		deck.cards.delete(card)
+		@card = @deck.cards.sample
+		@deck.cards.delete(card)
 		@turns += 1
 		puts card
+	end
 
+	def pick_a_card
+		while @turns < 3 and @game_end == false
+			puts "Press enter to receive a card."
+			answer = gets
+			deal if answer == "\n"
+			evaluate_card
+			win_or_lose
+		end
+		play_again_message
+	end	
+
+	def evaluate_card
 		if ["K", "Q", "J", "1"].include? card[0]
 			@counter += 10
 		elsif card[0] == "A"
-			if counter == 10
-				@counter += 11
-				puts "BLACKJACK!"
-				@game_end = true
-				return
-			elsif counter > 11
+			if counter > 11
 				@counter += 1
 			else
 				@counter += 11
@@ -71,7 +103,9 @@ class Blackjack
 		else
 			@counter += card[0].to_i
 		end
+	end
 
+	def win_or_lose
 		if @counter == 21
 			puts "BLACKJACK!"
 			@game_end = true
@@ -83,22 +117,14 @@ class Blackjack
 		end
 	end
 
-	def pick_a_card
-		while @turns < 3 and @game_end == false
-			puts "Press enter to receive a card."
-			answer = gets
-			deal if answer == "\n"
-		end
+	def play_again_message
+		puts "\nWould you like to play again?"
+		get_input_string
+		enter_yes_or_no
 		play_again
 	end
 
 	def play_again
-		puts "\nWould you like to play again?"
-		answer = gets.chomp.downcase
-		while ["yes", "y", "no", "n"].include?(answer) == false
-			puts "Please answer yes or no."
-			answer = gets.chomp.downcase
-		end
 		case answer
 			when "yes", "y"
 				@turns = 0
@@ -112,18 +138,23 @@ class Blackjack
 		end
 	end
 
-	attr_accessor :no_of_decks, :counter, :turns, :game_end
-
-	def initialize
-		@no_of_decks = 0
-		@counter = 0
-		@turns = 0
-		@game_end = false
-		
-		welcome
-		pick_a_card
-	end
-
 end
 
-Blackjack.new
+class Deck
+	attr_accessor :cards
+
+	SUITS = %w{ Clubs Diamonds Hearts Spades }
+	RANKS = %w{ 2 3 4 5 6 7 8 9 10 Jack Queen King Ace }
+
+	def initialize(n=1)
+		@cards = []
+		SUITS.cycle(n) do |s|
+			RANKS.each do |r|
+				@cards << "#{r} of #{s}"
+			end
+		end
+	end
+end
+
+game = Blackjack.new
+game.run
